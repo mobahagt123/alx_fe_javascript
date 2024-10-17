c89b64575de6159ca38ccfec74be3ea12b76b9f4
Mohamed Bahgat <52916078+mobahagt123@users.noreply.github.com>
2024-10-17 20:43:02 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,79 @@
+
+
+// fetch data from API and store at local storage
+
+async function getQuoates() {
+    const response = await fetch('quotes.json')
+    const data = await response.json()
+
+    // store quotes at local storage
+
+    localStorage.setItem('quotes', JSON.stringify(data))
+}
+
+
+function getAllQuoates(){
+
+    let myData = JSON.parse(localStorage.getItem('quotes'))
+    let myQuoates = Object.values(myData)
+    let allQuoates = []
+    for(let quote of myQuoates){
+        for (let quoteText of quote){
+            allQuoates.push(quoteText)
+        }
+    }
+    return allQuoates
+}
+
+
+function showRandomQuote(){
+
+    const quoteBtn  = document.getElementById('newQuote')
+    const diplayQuoat = document.getElementById('quoteDisplay')
+    const allQuoates = getAllQuoates()
+    
+    const blockQoate = document.createElement('p')
+    const cited = document.createElement('cite')
+
+    // add to div container
+    diplayQuoat.appendChild(blockQoate)
+    diplayQuoat.appendChild(cited)
+
+    quoteBtn.addEventListener('click' , (event) => {
+        const randomQuote = allQuoates[(Math.floor(Math.random() * allQuoates.length))]    
+        // add new text
+
+        blockQoate.textContent = `"${randomQuote['quote']}"`
+        cited.textContent = randomQuote['author']      
+    })
+
+    blockQoate.textContent = ' '
+    cited.textContent = ' '
+
+}
+
+function addQuote(){
+
+    // take user inputs for the newQuote
+    let quoteText = document.getElementById('newQuoteText').value
+    let quoteCategory= document.getElementById('newQuoteCategory').value
+    let quoteAuthor = document.getElementById('newQuoteAuthor').value
+    
+    // upload data from local storage
+
+    let qoutes = JSON.parse(localStorage.getItem('quotes'))
+
+    // adding new element
+    if (quoteAuthor && quoteCategory && quoteText){
+        qoutes[quoteCategory].push([{
+            quote: quoteText,
+            author: quoteAuthor
+        }])
+    }
+
+    // store quotes again
+    localStorage.setItem('quotes', JSON.stringify(qoutes))
+
+}
+
+showRandomQuote()
